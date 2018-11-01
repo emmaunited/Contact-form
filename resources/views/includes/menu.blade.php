7f94b6f59b3e2a2f52bd8cc3165076c4bf783a9b
Emmanuel Ajiboye <eoa22@njit.edu>
2018-11-01 16:51:41 -0400

    Task: Conatact form

---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">Boye</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="active"><a href="{{route ('index')}}">Home</a></li>
+            <li><a href="{{route ('about')}}">About</a>
+            </li>
+            <li><a href="{{route ('contact')}}">Contact</a>
+            </li>
+
+
+    </div>
+</nav>
